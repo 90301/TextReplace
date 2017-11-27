6137b44f7b6565e62ffd3bca3ab30e28e1b74290
Joshua Benton <tbsw_productions@hotmail.com>
2017-11-27 18:41:50 -0500

    Query Add table / where clause condition

--- ProgramableText/QueryPermuter.xaml.cs
+++ ProgramableText/QueryPermuter.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,8 @@ namespace ProgramableText
         List<String> tables = new List<string>();
         List<String> conditions = new List<string>();
         private String previewQuery = "";
+        private SqlConnection sqlConnection;
+
         public QueryPermuter()
         {
             InitializeComponent();
@@ -48,20 +51,57 @@ namespace ProgramableText
 
 
             tables.Add(tableToAdd);
+
+            updatePreviewQuery();
+
+            addTableJoinConditionTextBox.Clear();
+
         }
 
-        private void addConditionBtn1_Click(object sender, RoutedEventArgs e)
+        private void addConditionBtn_Click(object sender, RoutedEventArgs e)
         {
             String conditionToAdd = addConditionTextBox.Text;
             conditions.Add(conditionToAdd);
+            
+            updatePreviewQuery();
+            addTableTextBox.Clear();
+            addConditionTextBox.Clear();
+
         }
 
         public void updatePreviewQuery()
         {
             previewQuery = "Select count(1) from " + Environment.NewLine;
             previewQuery += tables.Select(x => x).Aggregate((i, j) => i + Environment.NewLine + j);
-            previewQuery += Environment.NewLine + " where " + Environment.NewLine;
-            previewQuery += conditions.Select(x => x).Aggregate((i, j) => i + Environment.NewLine + j);
+            if (conditions.Count >= 1)
+            {
+                previewQuery += Environment.NewLine + " where " + Environment.NewLine;
+                previewQuery += conditions.Select(x => x).Aggregate((i, j) => i + Environment.NewLine + j);
+            }
+
+            QueryPreviewTextBox.Text = previewQuery;
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            String connStr = "";
+            if (sqlServerConString.Text.Length > 5)
+            {
+                connStr = sqlServerConString.Text;
+            }
+            else
+            {
+                connStr = "Data Source="+ sqlserverTextbox.Text+
+                    ";Initial Catalog="+ sqlserverDBTextbox.Text +
+                    ";User ID=" + sqlserverUserTextbox.Text +
+                    ";Password=" + sqlserverPassTextbox.Text;
+                sqlServerConString.Text = connStr;
+            }
+
+            sqlConnection = new SqlConnection(connStr);
+            //sqlConnection.ConnectionString = connStr;
+            sqlConnection.Open();
         }
     }
 }
