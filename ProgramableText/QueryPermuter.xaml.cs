afd55235b7aee007042719c5b39c17c2191c9312
Joshua Benton <tbsw_productions@hotmail.com>
2017-11-29 14:50:16 -0500

    Query Permuter Functional, partial query add

--- ProgramableText/QueryPermuter.xaml.cs
+++ ProgramableText/QueryPermuter.xaml.cs
@@ -22,40 +22,85 @@ namespace ProgramableText
     {
         public String JOIN_KEYWORD = "JOIN";
         public String ON_KEYWORD = "ON";
-        List<String> tables = new List<string>();
-        List<String> conditions = new List<string>();
+        List<String> tables = new List<string>();//Tables and join conditions
+        List<String> conditions = new List<string>();//WHERE CONDITIONS
+        List<String> joinList = new List<string>();
         private String previewQuery = "";
         private SqlConnection sqlConnection;
+
+        
 
         public QueryPermuter()
         {
             InitializeComponent();
+
+            joinList.Clear();
+            joinList.Add("JOIN");
+            joinList.Add("FULL OUTER JOIN");
+            joinList.Add("INNER JOIN");
+            joinList.Add("LEFT JOIN");
+            joinList.Add("RIGHT JOIN");
         }
 
         private void addTableBtn_Click(object sender, RoutedEventArgs e)
         {
+            /*
             String tableToAdd = "";
-            if (tables.Count >= 1)
+            if (tableToAdd.Length > 1)
             {
-                //add join condition
-                tableToAdd += " " + JOIN_KEYWORD + " ";
+                tableToAdd += " ";
+            }
+            if (tables.Count >= 1 && !addTableTextBox.Text.Contains(JOIN_KEYWORD))
+            {
+                //add join condition (if it doesn't exist)
+                
+                tableToAdd += JOIN_KEYWORD + " ";
             }
             //table includes join condition for now
             tableToAdd += addTableTextBox.Text;
-
-            if (tables.Count >= 1)
+            */
+            /*
+            if (tables.Count >= 1 && addTableJoinConditionTextBox.Text.Length>1)
             {
-                //add join condition
+                //add join "ON" condition (if using the 2nd textbox)
                 tableToAdd += " " + ON_KEYWORD + " " + addTableJoinConditionTextBox.Text;
             }
+            */
 
+            //tables.Add(tableToAdd);
 
-            tables.Add(tableToAdd);
+            addTable(addTableTextBox.Text);
 
             updatePreviewQuery();
 
             addTableJoinConditionTextBox.Clear();
 
+        }
+
+        public void addTable(String text)
+        {
+            String tableToAdd = "";
+            if (tableToAdd.Length > 1)
+            {
+                tableToAdd += " ";
+            }
+            if (tables.Count >= 1 && !text.Contains(JOIN_KEYWORD))
+            {
+                //add join condition (if it doesn't exist)
+
+                tableToAdd += JOIN_KEYWORD + " ";
+            }
+            //table includes join condition for now
+            tableToAdd += text;
+
+            if (tables.Count >= 1 && addTableJoinConditionTextBox.Text.Length > 1)
+            {
+                //add join "ON" condition (if using the 2nd textbox)
+                tableToAdd += " " + ON_KEYWORD + " " + addTableJoinConditionTextBox.Text;
+            }
+
+
+            tables.Add(tableToAdd);
         }
 
         private void addConditionBtn_Click(object sender, RoutedEventArgs e)
@@ -82,6 +127,60 @@ namespace ProgramableText
             QueryPreviewTextBox.Text = previewQuery;
         }
 
+        public void tableJoinPermute()
+        {
+            
+
+            String startSQL = "Select count(1) from " + tables[0];
+            String firstJoin = tables[1];
+            List<String> tablesToJoin = new List<string>();
+            tablesToJoin.AddRange(tables.Skip(2));
+
+            tableJoinRecursion(startSQL,firstJoin,tablesToJoin);
+
+
+
+        }
+
+        public void tableJoinRecursion(String queryWithoutJoin,String tableToJoin,List<String> remainingTablesToJoin)
+        {
+            String nextJoin = "";
+            List<String> nextRemainingTables = new List<string>();
+            if (remainingTablesToJoin.Count >= 1)
+            {
+                nextJoin = remainingTablesToJoin[0];
+                for (int i = 1; i < remainingTablesToJoin.Count; i++)
+                {
+                    nextRemainingTables.Add(remainingTablesToJoin[i]);
+                }
+            }
+
+            foreach (String join in joinList)
+            {
+                string queryToRun = queryWithoutJoin + " "+ tableToJoin.Replace(JOIN_KEYWORD, join);
+                SqlCommand sql = new SqlCommand(queryToRun,sqlConnection);
+                
+                SqlDataReader reader = sql.ExecuteReader();
+                int count = 0;
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    count = reader.GetInt32(0);
+                }
+                reader.Close();
+                Console.WriteLine(count + queryToRun);
+
+                if (remainingTablesToJoin.Count >= 1)
+                {
+                    tableJoinRecursion(queryToRun, nextJoin,nextRemainingTables);
+                }
+
+
+            }
+        }
+
+        
+
         private void connectButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -107,6 +206,36 @@ namespace ProgramableText
         private void runButton_Click(object sender, RoutedEventArgs e)
         {
             //currently cycle through (IN ORDER) the table joins (with different styles)
+            tableJoinPermute();
+        }
+
+        private void clearQueryButton_Click(object sender, RoutedEventArgs e)
+        {
+            tables.Clear();
+            conditions.Clear();
+        }
+
+        private void partialQueryAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            String partialQuery = QueryPreviewTextBox.Text;
+
+            partialQuery = partialQuery.Replace("from", "").Replace("FROM","").Replace(Environment.NewLine," ");
+
+            foreach (String join in joinList)
+            {
+                partialQuery = partialQuery.Replace(join,JOIN_KEYWORD);
+            }
+
+            String[] splitQuery = partialQuery.Split(new string[] {JOIN_KEYWORD},StringSplitOptions.None);
+
+            foreach (string table in splitQuery)
+            {
+                addTable(table);
+            }
+
+            updatePreviewQuery();
+
+
         }
     }
 }
