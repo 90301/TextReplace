b36da79b511a6183d5c350b2c8ea071feee07453
Josh Benton <josh.benton.ce@gmail.com>
2019-11-20 15:52:01 -0500

    Extra Pass, Log Processor Seperation, Plus Base

---
+++ ProgramableText/LogProcessor/PlusBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramableText.LogProcessor
+{
+
+    class PlusBase : ProgramNode , ExtraPass
+    {
+        public String cachedText = "";
+        public override string calculate(string input)
+        {
+            cachedText = input;
+            LogProcessor.addProcessedExtraPass(this);
+            return LogProcessor.inputText;
+        }
+
+        public override ProgramNode createInstance()
+        {
+            return new PlusBase();
+        }
+
+        public override string getOpName()
+        {
+            return "plusBase";
+        }
+
+        public override void parseArgs(string[] args)
+        {
+            
+        }
+
+        public string extraPass(string input)
+        {
+            return cachedText + Environment.NewLine + input;
+        }
+
+        public override String ToString()
+        {
+            return getOpName() + "()";
+        }
+    }
+}
